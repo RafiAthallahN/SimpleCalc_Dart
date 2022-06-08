4ee67db8844d73f7d4e4e66047e15f27048626ba
RafiAthallahN <75708409+RafiAthallahN@users.noreply.github.com>
2022-06-08 08:38:35 +0700

    ok

---
+++ bin/simple_calcc.dart
@@ -0,0 +1,31 @@
+import 'package:simple_calcc/simple_calcc.dart' as simple_calcc;
+import 'dart:io';
+
+void main(List<String> arguments) {
+ stdout.write('Masukkan angka pertama : ');
+var firstNumber = num.parse(stdin.readLineSync()!);
+stdout.write('Masukkan operator [ + | - | * | / | % ] : ');
+var operator = stdin.readLineSync();
+stdout.write('Masukkan angka kedua : ');
+var secondNumber = num.parse(stdin.readLineSync()!);
+ 
+switch (operator) {
+  case '+':
+    print('$firstNumber $operator $secondNumber = ${firstNumber + secondNumber}');
+    break;
+  case '-':
+    print('$firstNumber $operator $secondNumber = ${firstNumber - secondNumber}');
+    break;
+  case '*':
+    print('$firstNumber $operator $secondNumber = ${firstNumber * secondNumber}');
+    break;
+  case '/':
+    print('$firstNumber $operator $secondNumber = ${firstNumber / secondNumber}');
+    break;
+  case '%':
+    print('$firstNumber $operator $secondNumber = ${firstNumber % secondNumber}');
+    break;
+  default:
+    print('Operator tidak ditemukan');
+}
+}
---
+++ test/simple_calcc_test.dart
@@ -0,0 +1,8 @@
+import 'package:simple_calcc/simple_calcc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
